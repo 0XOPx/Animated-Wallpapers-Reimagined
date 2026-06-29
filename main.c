bb58ddfd9bf4644a875dbde076bec58ec71e1535
OXOP <minbartek41@gmail.com>
2026-06-29 19:32:43 +0200

    we back baby v1.0.0.1

--- main.c
+++ main.c
@@ -5,11 +5,36 @@
 #include <strsafe.h>
 #include "config.h"
 
-IMFMediaEngine* g_pMediaEngine = NULL;
+#define OXOP_MAX_MONITORS 16
+
+HWND g_hwnds[OXOP_MAX_MONITORS];
+IMFMediaEngine* g_engines[OXOP_MAX_MONITORS];
+IMFAttributes* g_attributes[OXOP_MAX_MONITORS];
+int g_monitorCount = 0;
+
+HINSTANCE g_hInstance = NULL;
+IMFMediaEngineClassFactory* g_pFactory = NULL;
+wchar_t g_videoPath[MAX_PATH];
 
 BOOL EnsureSingleInstance(void) {
     CreateMutexW(NULL, TRUE, MUTEX_NAME);
     return (GetLastError() != ERROR_ALREADY_EXISTS);
+}
+
+BOOL CALLBACK KillOldInstancesProc(HWND hwnd, LPARAM lParam) {
+    wchar_t className[256];
+    if (GetClassNameW(hwnd, className, 256) && lstrcmpW(className, WEBM_WINDOW_CLASS) == 0) {
+        DWORD pid = 0;
+        GetWindowThreadProcessId(hwnd, &pid);
+        if (pid != 0 && pid != GetCurrentProcessId()) {
+            HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, pid);
+            if (hProcess) {
+                TerminateProcess(hProcess, 0);
+                CloseHandle(hProcess);
+            }
+        }
+    }
+    return TRUE;
 }
 
 BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam) {
@@ -31,6 +56,7 @@ HWND GetWorkerW(void) {
 typedef struct {
     IMFMediaEngineNotify Vtbl;
     long m_cRef;
+    IMFMediaEngine* pEngine; 
 } MediaEngineNotifyC;
 
 STDMETHODIMP QueryInterfaceC(IMFMediaEngineNotify* thisPtr, REFIID riid, void** ppvObject) {
@@ -49,10 +75,12 @@ STDMETHODIMP_(ULONG) ReleaseC(IMFMediaEngineNotify* thisPtr) {
     if (cRef == 0) HeapFree(GetProcessHeap(), 0, thisPtr);
     return cRef;
 }
+
 STDMETHODIMP EventNotifyC(IMFMediaEngineNotify* thisPtr, DWORD event, DWORD_PTR param1, DWORD param2) {
-    if (event == MF_MEDIA_ENGINE_EVENT_ENDED && g_pMediaEngine) {
-        IMFMediaEngine_SetCurrentTime(g_pMediaEngine, 0.0);
-        IMFMediaEngine_Play(g_pMediaEngine);
+    MediaEngineNotifyC* pThis = (MediaEngineNotifyC*)thisPtr;
+    if (event == MF_MEDIA_ENGINE_EVENT_ENDED && pThis->pEngine) {
+        IMFMediaEngine_SetCurrentTime(pThis->pEngine, 0.0);
+        IMFMediaEngine_Play(pThis->pEngine);
     }
     return S_OK;
 }
@@ -66,19 +94,73 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
     return DefWindowProcW(hwnd, uMsg, wParam, lParam);
 }
 
+BOOL CALLBACK MonitorEnumProc(HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMonitor, LPARAM dwData) {
+    if (g_monitorCount >= OXOP_MAX_MONITORS) return FALSE;
+
+    int x = lprcMonitor->left;
+    int y = lprcMonitor->top;
+    int w = lprcMonitor->right - lprcMonitor->left;
+    int h = lprcMonitor->bottom - lprcMonitor->top;
+
+    HWND hwnd = CreateWindowExW(0, WEBM_WINDOW_CLASS, L"Animated Wallpapers Reimagined", 
+                               WS_POPUP | WS_VISIBLE, x, y, w, h, NULL, NULL, g_hInstance, NULL);
+
+    HWND workerw = (HWND)dwData;
+    if (workerw) SetParent(hwnd, workerw);
+
+    MediaEngineNotifyC* pNotify = (MediaEngineNotifyC*)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(MediaEngineNotifyC));
+    pNotify->Vtbl.lpVtbl = &g_NotifyVtbl;
+    pNotify->m_cRef = 1;
+
+    IMFAttributes* pAttributes = NULL;
+    MFCreateAttributes(&pAttributes, 3);
+    
+    IMFAttributes_SetUnknown(pAttributes, &MF_MEDIA_ENGINE_CALLBACK, (IUnknown*)pNotify);
+    IMFAttributes_SetUINT64(pAttributes, &MF_MEDIA_ENGINE_PLAYBACK_HWND, (UINT64)hwnd);
+    IMFAttributes_SetUINT32(pAttributes, &MF_MEDIA_ENGINE_VIDEO_OUTPUT_FORMAT, DXGI_FORMAT_B8G8R8A8_UNORM);
+
+    IMFMediaEngine* pEngine = NULL;
+    IMFMediaEngineClassFactory_CreateInstance(g_pFactory, 0, pAttributes, &pEngine);
+
+    if (pEngine) {
+        pNotify->pEngine = pEngine;
+
+        BSTR bstrURL = SysAllocString(g_videoPath);
+        IMFMediaEngine_SetSource(pEngine, bstrURL);
+        IMFMediaEngine_Play(pEngine);
+        SysFreeString(bstrURL);
+
+        g_hwnds[g_monitorCount] = hwnd;
+        g_engines[g_monitorCount] = pEngine;
+        g_attributes[g_monitorCount] = pAttributes;
+        g_monitorCount++;
+    } else {
+        IUnknown_Release(pAttributes);
+        HeapFree(GetProcessHeap(), 0, pNotify);
+        DestroyWindow(hwnd);
+    }
+
+    return TRUE;
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    wchar_t lockPath[MAX_PATH];
+    GetModuleFileNameW(NULL, lockPath, MAX_PATH);
+    wchar_t* lastSlash = wcsrchr(lockPath, L'\\');
+    if (lastSlash) *(lastSlash + 1) = L'\0';
+    StringCchCatW(lockPath, MAX_PATH, L".LOCK");
+
+    if (GetFileAttributesW(lockPath) != INVALID_FILE_ATTRIBUTES) {
+        EnumWindows(KillOldInstancesProc, 0);
+        Sleep(100);
+    }
+
     if (!EnsureSingleInstance()) return 0;
 
+    g_hInstance = hInstance;
+
     CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
-
-#if defined(_MSC_VER)
-    MFStartup(MF_VERSION, MFSTARTUP_FULL);
-#else
     MFStartup(MF_VERSION, MFSTARTUP_FULL); 
-#endif
-
-    int sw = GetSystemMetrics(SM_CXSCREEN);
-    int sh = GetSystemMetrics(SM_CYSCREEN);
 
     WNDCLASSW wc = {0};
     wc.lpfnWndProc = WindowProc;
@@ -86,37 +168,16 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     wc.lpszClassName = WEBM_WINDOW_CLASS;
     RegisterClassW(&wc);
 
-    HWND hwnd = CreateWindowExW(0, WEBM_WINDOW_CLASS, L"Animated Wallpapers Reimagined", 
-                               WS_POPUP | WS_VISIBLE, 0, 0, sw, sh, NULL, NULL, hInstance, NULL);
+    GetModuleFileNameW(NULL, g_videoPath, MAX_PATH);
+    lastSlash = wcsrchr(g_videoPath, L'\\');
+    if (lastSlash) *(lastSlash + 1) = L'\0';
+    StringCchCatW(g_videoPath, MAX_PATH, DEFAULT_VIDEO_NAME);
+
+    CoCreateInstance(&CLSID_MFMediaEngineClassFactory, NULL, CLSCTX_INPROC_SERVER, &IID_IMFMediaEngineClassFactory, (void**)&g_pFactory);
 
     HWND workerw = GetWorkerW();
-    if (workerw) SetParent(hwnd, workerw);
 
-    IMFMediaEngineClassFactory* pFactory = NULL;
-    IMFAttributes* pAttributes = NULL;
-    MediaEngineNotifyC* pNotify = (MediaEngineNotifyC*)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(MediaEngineNotifyC));
-    pNotify->Vtbl.lpVtbl = &g_NotifyVtbl;
-    pNotify->m_cRef = 1;
-
-    CoCreateInstance(&CLSID_MFMediaEngineClassFactory, NULL, CLSCTX_INPROC_SERVER, &IID_IMFMediaEngineClassFactory, (void**)&pFactory);
-    MFCreateAttributes(&pAttributes, 3);
-    
-    IMFAttributes_SetUnknown(pAttributes, &MF_MEDIA_ENGINE_CALLBACK, (IUnknown*)pNotify);
-    IMFAttributes_SetUINT64(pAttributes, &MF_MEDIA_ENGINE_PLAYBACK_HWND, (UINT64)hwnd);
-    IMFAttributes_SetUINT32(pAttributes, &MF_MEDIA_ENGINE_VIDEO_OUTPUT_FORMAT, DXGI_FORMAT_B8G8R8A8_UNORM);
-
-    IMFMediaEngineClassFactory_CreateInstance(pFactory, 0, pAttributes, &g_pMediaEngine);
-
-    wchar_t videoPath[MAX_PATH];
-    GetModuleFileNameW(NULL, videoPath, MAX_PATH);
-    wchar_t* lastSlash = wcsrchr(videoPath, L'\\');
-    if (lastSlash) *(lastSlash + 1) = L'\0';
-    StringCchCatW(videoPath, MAX_PATH, DEFAULT_VIDEO_NAME);
-
-    BSTR bstrURL = SysAllocString(videoPath);
-    IMFMediaEngine_SetSource(g_pMediaEngine, bstrURL);
-    IMFMediaEngine_Play(g_pMediaEngine);
-    SysFreeString(bstrURL);
+    EnumDisplayMonitors(NULL, NULL, MonitorEnumProc, (LPARAM)workerw);
 
     MSG msg = {0};
     while (GetMessageW(&msg, NULL, 0, 0)) {
@@ -124,9 +185,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         DispatchMessageW(&msg);
     }
 
-    if (g_pMediaEngine) IUnknown_Release(g_pMediaEngine);
-    IUnknown_Release(pAttributes);
-    IUnknown_Release(pFactory);
+    for (int i = 0; i < g_monitorCount; i++) {
+        if (g_engines[i]) IUnknown_Release(g_engines[i]);
+        if (g_attributes[i]) IUnknown_Release(g_attributes[i]);
+    }
+    
+    if (g_pFactory) IUnknown_Release(g_pFactory);
 
     MFShutdown();
     CoUninitialize();
---
+++ setup.iss
@@ -0,0 +1,24 @@
+[Setup]
+AppId={{A3F1B8C9-2D4E-461A-8B7C-9E0F1A2B3C4D}
+AppName=Animated Wallpapers Reimagined
+AppVersion=1.0.0.0
+AppPublisher=OXOP
+DefaultDirName={userappdata}\AnimatedWallpapersReimagined
+DefaultGroupName=Animated Wallpapers Reimagined
+DisableProgramGroupPage=yes
+PrivilegesRequired=lowest
+OutputBaseFilename=Animated_Wallpapers_Reimagined_Setup
+Compression=lzma2
+SolidCompression=yes
+ArchitecturesInstallIn64BitMode=x64
+
+[Files]
+Source: "Animated Wallpapers Reimagined.exe"; DestDir: "{app}"; Flags: ignoreversion
+Source: "video.mp4"; DestDir: "{app}"; Flags: ignoreversion
+
+[Run]
+Filename: "schtasks.exe"; Parameters: "/create /f /tn ""AnimatedWallpapersReimagined"" /tr ""\""{app}\Animated Wallpapers Reimagined.exe\"""" /sc onlogon"; Flags: runhidden
+Filename: "{app}\Animated Wallpapers Reimagined.exe"; Description: "{cm:LaunchProgram,Animated Wallpapers Reimagined}"; Flags: nowait postinstall skipifsilent
+
+[UninstallRun]
+Filename: "schtasks.exe"; Parameters: "/delete /f /tn ""AnimatedWallpapersReimagined"""; Flags: runhidden
